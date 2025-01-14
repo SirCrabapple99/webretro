99371caae6ab93222cbc23cebca4a8210d1d6574
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-01-14 13:44:15 -0600

    Update tokens.js

--- uauth/tokens.js
+++ uauth/tokens.js
@@ -1,11 +1,11 @@
-// Google Drive
+// Google Drive (not active on this site)
 var googleProjectId = "hallowed-digit-311307";
 var googleApiKey = "AIzaSyDuKbs32MBOZw87EJi0dsPVNHx6D4_QTOQ";
 var googleOauthClientId = "478810195063-c83gm38hf14o08gg4oqggql7pp9dqbdl.apps.googleusercontent.com";
 // scope: https://www.googleapis.com/auth/drive.readonly
 
-// Dropbox
+// Dropbox (active)
 var dropboxAppKey = "92l0dp1nkx6820o";
 
-// OneDrive
+// OneDrive (not active on this site)
 var onedriveClientId = "31614988-7150-44ca-b17f-d18e3ea1d3af";
